2985b1d35948ebde78d4494c5f8741c20995e83b
Sieghart Saladin <hisokamorow786@gmail.com>
2025-12-16 21:33:46 -0500

    feat(backend):se registro módulo de organizaciones

--- backend/src/modules/organizations/organizations.module.ts
+++ backend/src/modules/organizations/organizations.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
 import { Company } from './entities/company.entity';
+import { IamModule } from '../iam/iam.module';
+import { UserRole } from '../iam/entities/user-role.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([
-            Company,
-        ]),
+        TypeOrmModule.forFeature([Company, UserRole]),
+        IamModule
     ],
     controllers: [OrganizationsController],
     providers: [OrganizationsService],

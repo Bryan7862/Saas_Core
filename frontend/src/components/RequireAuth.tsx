c203f2f1b3331ae95d565bfb8693138494f87540
Sieghart Saladin <hisokamorow786@gmail.com>
2025-12-18 21:27:04 -0500

    feat(frontend-auth):se agrego componente RequireAuth para proteger rutas

---
+++ frontend/src/components/RequireAuth.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+export function RequireAuth({ children }: { children: JSX.Element }) {
+    const token = localStorage.getItem('access_token');
+    const location = useLocation();
+
+    if (!token) {
+        // Redirect them to the /login page, but save the current location they were
+        // trying to go to when they were redirected. This allows us to send them
+        // along to that page after they login, which is a nicer user experience.
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
+}

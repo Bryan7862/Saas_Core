8e52cf55a3c8b61753ea44e272e2794cb391e632
Sieghart Saladin <hisokamorow786@gmail.com>
2025-12-27 20:40:37 -0500

    tarea(orgs):se registro la entidad y el controlador de configuración en el módulo

--- backend/src/modules/organizations/organizations.module.ts
+++ backend/src/modules/organizations/organizations.module.ts
@@ -2,18 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
+import { OrganizationSettingsController } from './organization-settings.controller';
 import { Company } from './entities/company.entity';
 import { UserRole } from '../iam/entities/user-role.entity';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { IamModule } from '../iam/iam.module';
 
+import { OrganizationSettings } from './entities/organization-settings.entity';
+
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Company, UserRole]),
+        TypeOrmModule.forFeature([Company, UserRole, OrganizationSettings]),
         IamModule,
         SubscriptionsModule,
     ],
-    controllers: [OrganizationsController],
+    controllers: [OrganizationsController, OrganizationSettingsController],
     providers: [OrganizationsService],
     exports: [OrganizationsService], // Export so AuthModule can use it
 })

89ddc0daa1d9077e1e1257d870f3485ee7154eeb
Sieghart Saladin <hisokamorow786@gmail.com>
2025-12-27 20:34:05 -0500

    feat(pagos):para registrar el controlador de webhook e integrar la auditoría de transacciones

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PaymentsController } from './payments.controller';
 import { CulqiService } from './providers/culqi.service';
+import { TransactionsModule } from '../transactions/transactions.module';
+import { CulqiWebhookController } from './webhooks/culqi-webhook.controller';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 
 @Module({
-    imports: [ConfigModule, SubscriptionsModule],
-    controllers: [PaymentsController], // Removed StripeWebhookController for now
+    imports: [TransactionsModule, SubscriptionsModule],
+    controllers: [PaymentsController, CulqiWebhookController],
     providers: [CulqiService],
+    exports: [CulqiService],
 })
 export class PaymentsModule { }
